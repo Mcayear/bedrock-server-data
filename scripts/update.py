614d1bd6ed0dec7a3116f1bfe0b7a5acdf758bf6
Vincent <magicdroidx@gmail.com>
2025-06-15 13:06:25 +0100

    feat: update script to use new API

--- scripts/update.py
+++ scripts/update.py
@@ -3,48 +3,71 @@ import json
 import logging
 import re
 from enum import Enum
-from packaging.version import Version
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from typing import List
 
 import requests
-from bs4 import BeautifulSoup
+from packaging.version import Version
+from pydantic import BaseModel, Field, HttpUrl
 from tqdm import tqdm
 
-
-class BuildType(Enum):
-    RELEASE = "release"
-    PREVIEW = "preview"
-
-
-class Platform(Enum):
-    WINDOWS = "windows"
-    LINUX = "linux"
-
-
-URL = "https://www.minecraft.net/en-us/download/server/bedrock"
+BASE_URL = "https://net-secondary.web.minecraft-services.net"
+API_PATH = "/api/v1.0/download/links"
+FULL_URL = f"{BASE_URL}{API_PATH}"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
 }
 
 
-def get_download_url(build_type: BuildType, platform: Platform) -> str:
-    response = requests.get(URL, headers=HEADERS)
-    soup = BeautifulSoup(response.content, "html.parser")
+class ServerType(str, Enum):
+    SERVER_BEDROCK_WINDOWS = "serverBedrockWindows"
+    SERVER_BEDROCK_LINUX = "serverBedrockLinux"
+    SERVER_BEDROCK_PREVIEW_WINDOWS = "serverBedrockPreviewWindows"
+    SERVER_BEDROCK_PREVIEW_LINUX = "serverBedrockPreviewLinux"
+    SERVER_JAR = "serverJar"
 
-    platform_map = {
-        (BuildType.RELEASE, Platform.WINDOWS): "serverBedrockWindows",
-        (BuildType.RELEASE, Platform.LINUX): "serverBedrockLinux",
-        (BuildType.PREVIEW, Platform.WINDOWS): "serverBedrockPreviewWindows",
-        (BuildType.PREVIEW, Platform.LINUX): "serverBedrockPreviewLinux",
-    }
+    @property
+    def is_bedrock(self) -> bool:
+        return "Bedrock" in self.value
 
-    link = soup.find("a", {"data-platform": platform_map[(build_type, platform)]})
-    if link:
-        return link.get("href")
-    raise ValueError(
-        f"Could not find download URL for {build_type.value} {platform.value}"
-    )
+    @property
+    def is_preview(self) -> bool:
+        return "Preview" in self.value
+
+    @property
+    def channel(self) -> str:
+        if "serverBedrockPreview" in self.value:
+            return "preview"
+        elif "serverBedrock" in self.value:
+            return "release"
+        else:
+            raise NotImplementedError
+
+    @property
+    def platform(self) -> str:
+        if "Windows" in self.value:
+            return "windows"
+        elif "Linux" in self.value:
+            return "linux"
+        else:
+            raise NotImplementedError
+
+
+class DownloadLink(BaseModel):
+    download_type: ServerType = Field(..., alias="downloadType")
+    download_url: HttpUrl = Field(..., alias="downloadUrl")
+
+
+class DownloadLinksResult(BaseModel):
+    links: List[DownloadLink]
+
+
+def get_download_links() -> dict[ServerType, HttpUrl]:
+    response = requests.get(FULL_URL, headers=HEADERS)
+    response.raise_for_status()
+    result = DownloadLinksResult(**response.json()["result"])
+    return {link.download_type: link.download_url for link in result.links}
 
 
 def download_file(url, filename):
@@ -73,7 +96,7 @@ def compute_checksum(filename):
     return hashlib.sha256(file_data).hexdigest()
 
 
-def update_versions_file(build_type: BuildType, version: str):
+def update_versions_file(server_type: ServerType, version: str):
     version_file = Path("versions.json")
     if version_file.exists():
         with version_file.open(mode="r") as f:
@@ -81,20 +104,19 @@ def update_versions_file(build_type: BuildType, version: str):
     else:
         data = {"release": {"latest": "", "versions": []}, "preview": {"latest": "", "versions": []}}
 
-    versions_list = data[build_type.value]["versions"]
+    versions_list = data[server_type.channel]["versions"]
     if version not in versions_list:
         versions_list.insert(0, version)  # Add new version at the beginning
-        data[build_type.value]["latest"] = version  # Update latest
+        data[server_type.channel]["latest"] = version  # Update latest
 
     with version_file.open(mode="w") as f:
         json.dump(data, f, indent=2)
 
-    logging.info(f"Updated {version_file} with version {version} for {build_type.value}")
+    logging.info(f"Updated {version_file} with version {version} for {server_type.value}")
 
 
-def process(build_type: BuildType, platform: Platform):
-    logging.info(f"Processing {build_type.value} build for {platform.value}...")
-    url = get_download_url(build_type, platform)
+def process(server_type: ServerType, url: str):
+    logging.info(f"Processing {server_type.value} build...")
 
     regex = r"bedrock-server-(\d+\.\d+(?:\.\d+){1,2})\.zip"
     match = re.search(regex, url)
@@ -103,15 +125,15 @@ def process(build_type: BuildType, platform: Platform):
 
     version_parts = Version(match.group(1)).release
     version = "{}.{}.{}{}".format(
-        *version_parts[:3], "" if build_type == BuildType.RELEASE else f"-preview.{version_parts[3]}"
+        *version_parts[:3], f"-preview.{version_parts[3]}" if server_type.is_preview else ""
     )
 
     with TemporaryDirectory() as tmp:
-        filename = Path(tmp) / f"bedrock_server_{platform.value}.zip"
+        filename = Path(tmp) / f"bedrock_server.zip"
         download_file(url, filename)
         checksum = compute_checksum(filename)
 
-    version_dir = Path(".") / build_type.value / version
+    version_dir = Path(".") / server_type.channel / version
     version_dir.mkdir(parents=True, exist_ok=True)
 
     metadata_path = version_dir / "metadata.json"
@@ -125,7 +147,7 @@ def process(build_type: BuildType, platform: Platform):
     if "binary" not in metadata:
         metadata["binary"] = {}
 
-    metadata["binary"][platform.value] = {
+    metadata["binary"][server_type.platform] = {
         "url": url,
         "sha256": checksum,
     }
@@ -135,24 +157,22 @@ def process(build_type: BuildType, platform: Platform):
 
     logging.info(f"Saved metadata for {version} in {metadata_path}")
 
-    update_versions_file(build_type, version)
+    update_versions_file(server_type, version)
     return version
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    for build_type in BuildType:
-        for platform in Platform:
-            try:
-                version = process(build_type, platform)
-                logging.info(
-                    f"Processed version: {version} ({build_type.value}) for {platform.value}"
-                )
-            except Exception as e:
-                logging.error(
-                    f"Failed to process {build_type.value} build for {platform.value}: {e}"
-                )
+    download_links = get_download_links()
+    for server_type, url in download_links.items():
+        if not server_type.is_bedrock:
+            continue
+
+        version = process(server_type, str(url))
+        logging.info(
+            f"Processed version: {version} ({server_type.value})"
+        )
 
 
 if __name__ == "__main__":
